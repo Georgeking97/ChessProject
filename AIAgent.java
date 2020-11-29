30f8f28f6e165d21102dee832450e4f4b3ce1c13
Georgeking97 <44296390+Georgeking97@users.noreply.github.com>
2020-11-29 22:00:36 +0000

    Current state, GUI done, unsure of where to go next

---
+++ AIAgent.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class AIAgent extends ChessProject{
+    Random rand;
+
+    public AIAgent(){
+        rand = new Random();
+    }
+
+    //pass in the stack
+
+/*
+  The method randomMove takes as input a stack of potential moves that the AI agent
+  can make. The agent uses a random number generator to randomly select a move from
+  the inputted Stack and returns this to the calling agent.
+*/
+
+    public Move randomMove(Stack possibilities){
+
+        int moveID = rand.nextInt(possibilities.size());
+        System.out.println("Agent randomly selected move : "+moveID);
+        for(int i=1;i < (possibilities.size()-(moveID));i++){
+            possibilities.pop();
+        }
+        Move selectedMove = (Move)possibilities.pop();
+        return selectedMove;
+    }
+
+    public Move nextBestMove(Stack possibilities){
+        Move selectedMove = new Move();
+        return selectedMove;
+    }
+
+    public Move twoLevelsDeep(Stack possibilities){
+        Move selectedMove = new Move();
+        return selectedMove;
+    }
+}
